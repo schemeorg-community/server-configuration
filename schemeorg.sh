e260b96d8610ede94eaf02e8fe4ff0b19a1db5f2
Lassi Kortela <lassi@lassi.io>
2023-03-02 12:23:22 +0200

    Rename schemeorg.scm -> schemeorg.pose
    Portable S-expressions

--- schemeorg.sh
+++ schemeorg.sh
@@ -7,7 +7,7 @@ mkdir -p schemeorg
 (cd lander && git describe --dirty --always) >schemeorg/lander-version.text
 cd schemeorg
 echo "Entering directory '$PWD'"
-$R7RS ../lander/lander.scm <../schemeorg.scm
+$R7RS ../lander/lander.scm <../schemeorg.pose
 mkdir -p roles/configure_nginx/files
 $R7RS ../nginx.scm >roles/configure_nginx/files/nginx.conf
 mkdir -p roles/make_production_api/files
