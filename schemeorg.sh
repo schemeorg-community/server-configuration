6ad8deba0a021d6660a6514dadb6da22efbe8ce1
Lassi Kortela <lassi@lassi.io>
2023-03-02 14:37:20 +0200

    Switch from Lander to Sensible
    https://github.com/lassik/sensible

--- schemeorg.sh
+++ schemeorg.sh
@@ -2,12 +2,9 @@
 set -eu
 R7RS=${R7RS:-chibi-scheme}
 cd "$(dirname "$0")"
-test -e lander || git clone https://github.com/lispunion/lander.git
-mkdir -p schemeorg
-(cd lander && git describe --dirty --always) >schemeorg/lander-version.text
+sensible -o schemeorg schemeorg.pose
 cd schemeorg
 echo "Entering directory '$PWD'"
-$R7RS ../lander/lander.scm <../schemeorg.pose
 mkdir -p roles/configure_nginx/files
 $R7RS ../nginx.scm >roles/configure_nginx/files/nginx.conf
 mkdir -p roles/make_production_api/files
@@ -15,4 +12,4 @@ mkdir -p roles/make_staging_api/files
 mkdir -p roles/set_server_basics/files
 { echo && cat ../motd.text && echo; } >roles/set_server_basics/files/motd
 $R7RS ../services.scm
-ansible-playbook schemeorg.yml "$@"
+ansible-playbook schemeorg.yaml "$@"
