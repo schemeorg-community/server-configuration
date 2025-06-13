378932cae69baf1d297dabc50cbbbca7a2e235bb
retropikzel <retropikzel@iki.fi>
2025-06-13 18:17:04 +0300

    Automatically reload configuratioN

--- jenkins/update.sh
+++ jenkins/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git pull
-curl \
-    -X POST \
-    -u ${JENKINS_USER}:${JENKINS_API_TOKEN} \
-    "https://jenkins.scheme.org/reload-configuration-as-code/"
+curl --netrc-file ${HOME}/.netrc \
+   -X POST \
+   -G -d @${HOME}/.casc-reload-token \
+   https://jenkins.scheme.org/reload-configuration-as-code/
 
